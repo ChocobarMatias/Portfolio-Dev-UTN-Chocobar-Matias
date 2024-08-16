d6bd694f29e326fbc3c9484e00de717434f8d259
Matias Chocobar <111932821+ChocobarMatias@users.noreply.github.com>
2024-08-16 05:44:54 -0300

    Update constants.js

--- Front-End/src/constants/constants.js
+++ Front-End/src/constants/constants.js
@@ -3,61 +3,61 @@
 //URL de Tablas
 
 //Administradores
-export const URL_ADMINISTRADORES = "http://localhost:8000/administradores"
-export const URL_ADMINISTRADORES_CREAR = "http://localhost:8000/administradores/create/"
-export const URL_ADMINISTRADORES_EDITAR = "http://localhost:8000/administradores/edit/"
-export const URL_ADMINISTRADORES_ELIMINAR = "http://localhost:8000/administradores/delete/"
+export const URL_ADMINISTRADORES = "http://localhost:3306/administradores"
+export const URL_ADMINISTRADORES_CREAR = "http://localhost:3306/administradores/create/"
+export const URL_ADMINISTRADORES_EDITAR = "http://localhost:3306/administradores/edit/"
+export const URL_ADMINISTRADORES_ELIMINAR = "http://localhost:3306/administradores/delete/"
 
 //Certificaciones
 export const URL_CERTIFICACIONES = "http://localhost:3306/certificados/"
-export const URL_CERTIFICACIONES_CREAR = "http://localhost:8000/certificados/create/"
-export const URL_CERTIFICACIONES_EDITAR = "http://localhost:8000/certificados/edit/"
-export const URL_CERTIFICACIONES_ELIMINAR = "http://localhost:8000/certificados/delete/"
+export const URL_CERTIFICACIONES_CREAR = "http://localhost:3306/certificados/create/"
+export const URL_CERTIFICACIONES_EDITAR = "http://localhost:3306/certificados/edit/"
+export const URL_CERTIFICACIONES_ELIMINAR = "http://localhost:3306/certificados/delete/"
 
 //Contactame
-export const URL_CONTACTAME = "http://localhost:8000/contactame"
-export const URL_CONTACTAME_CREAR = "http://localhost:8000/contactame/create/"
-export const URL_CONTACTAME_EDITAR = "http://localhost:8000/contactame/edit/"
-export const URL_CONTACTAME_ELIMINAR = "http://localhost:8000/contactame/delete/"
+export const URL_CONTACTAME = "http://localhost:3306/contactame"
+export const URL_CONTACTAME_CREAR = "http://localhost:3306/contactame/create/"
+export const URL_CONTACTAME_EDITAR = "http://localhost:3306/contactame/edit/"
+export const URL_CONTACTAME_ELIMINAR = "http://localhost:3306/contactame/delete/"
 
 //Experiencias
-export const URL_EXPERIENCIAS = "http://localhost:8000/experiencias"
-export const URL_EXPERIENCIAS_CREAR = "http://localhost:8000/experiencias/create/"
-export const URL_EXPERIENCIAS_EDITAR = "http://localhost:8000/experiencias/edit/"
-export const URL_EXPERIENCIAS_ELIMINAR = "http://localhost:8000/experiencias/delete/"
+export const URL_EXPERIENCIAS = "http://localhost:3306/experiencias"
+export const URL_EXPERIENCIAS_CREAR = "http://localhost:3306/experiencias/create/"
+export const URL_EXPERIENCIAS_EDITAR = "http://localhost:3306/experiencias/edit/"
+export const URL_EXPERIENCIAS_ELIMINAR = "http://localhost:3306/experiencias/delete/"
 
 //Formaciones
-export const URL_FORMACIONES = "http://localhost:8000/formaciones"
-export const URL_FORMACIONES_CREAR = "http://localhost:8000/formaciones/create/"
-export const URL_FORMACIONES_EDITAR = "http://localhost:8000/formaciones/edit/"
-export const URL_FORMACIONES_ELIMINAR = "http://localhost:8000/formaciones/delete/"
+export const URL_FORMACIONES = "http://localhost:3306/formaciones"
+export const URL_FORMACIONES_CREAR = "http://localhost:3306/formaciones/create/"
+export const URL_FORMACIONES_EDITAR = "http://localhost:3306/formaciones/edit/"
+export const URL_FORMACIONES_ELIMINAR = "http://localhost:3306/formaciones/delete/"
 
 //Idiomas
-export const URL_IDIOMAS = "http://localhost:8000/idiomas"
-export const URL_IDIOMAS_CREAR = "http://localhost:8000/idiomas/create/"
-export const URL_IDIOMAS_EDITAR = "http://localhost:8000/idiomas/edit/"
-export const URL_IDIOMAS_ELIMINAR = "http://localhost:8000/idiomas/delete/"
+export const URL_IDIOMAS = "http://localhost:3306/idiomas"
+export const URL_IDIOMAS_CREAR = "http://localhost:3306/idiomas/create/"
+export const URL_IDIOMAS_EDITAR = "http://localhost:3306/idiomas/edit/"
+export const URL_IDIOMAS_ELIMINAR = "http://localhost:3306/idiomas/delete/"
 
 //Proyectos
-export const URL_PROYECTOS = "http://localhost:8000/proyectos"
-export const URL_PROYECTOS_CREAR = "http://localhost:8000/proyectos/create/"
-export const URL_PROYECTOS_EDITAR = "http://localhost:8000/proyectos/edit/"
-export const URL_PROYECTOS_ELIMINAR = "http://localhost:8000/proyectos/delete/"
+export const URL_PROYECTOS = "http://localhost:3306/proyectos"
+export const URL_PROYECTOS_CREAR = "http://localhost:3306/proyectos/create/"
+export const URL_PROYECTOS_EDITAR = "http://localhost:3306/proyectos/edit/"
+export const URL_PROYECTOS_ELIMINAR = "http://localhost:3306/proyectos/delete/"
 
 //Skills
-export const URL_SKILLS = "http://localhost:8000/skills"
-export const URL_SKILLS_CREAR = "http://localhost:8000/skills/create/"
-export const URL_SKILLS_EDITAR = "http://localhost:8000/skills/edit/"
-export const URL_SKILLS_ELIMINAR = "http://localhost:8000/skills/delete/"
+export const URL_SKILLS = "http://localhost:3306/skills"
+export const URL_SKILLS_CREAR = "http://localhost:3306/skills/create/"
+export const URL_SKILLS_EDITAR = "http://localhost:3306/skills/edit/"
+export const URL_SKILLS_ELIMINAR = "http://localhost:3306/skills/delete/"
 
 //Usuarios
-export const URL_USUARIOS = "http://localhost:8000/usuarios"
-export const URL_USUARIOS_CREAR = "http://localhost:8000/usuarios/create/"
-export const URL_USUARIOS_EDITAR = "http://localhost:8000/usuarios/edit/"
-export const URL_USUARIOS_ELIMINAR = "http://localhost:8000/usuarios/delete/"
+export const URL_USUARIOS = "http://localhost:3306/usuarios"
+export const URL_USUARIOS_CREAR = "http://localhost:3306/usuarios/create/"
+export const URL_USUARIOS_EDITAR = "http://localhost:3306/usuarios/edit/"
+export const URL_USUARIOS_ELIMINAR = "http://localhost:3306/usuarios/delete/"
 
 // Login
-export const URL_LOGIN = "http://localhost:8000/login"
+export const URL_LOGIN = "http://localhost:3306/login"
 
 // Recuperar contraseña
-export const URL_RECUPERAR = "http://localhost:8000/recuperar"
+export const URL_RECUPERAR = "http://localhost:3306/recuperar"
