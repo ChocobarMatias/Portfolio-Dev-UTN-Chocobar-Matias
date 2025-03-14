bd35322e077cd12cfaa2762ae796c7030e61d536
Matias Sebastian Chocobar <111932821+ChocobarMatias@users.noreply.github.com>
2025-03-14 06:00:46 -0300

    Update constants.js

--- Front-End/src/constants/constants.js
+++ Front-End/src/constants/constants.js
@@ -3,61 +3,61 @@
 //URL de Tablas
 
 //Administradores
-export const URL_ADMINISTRADORES = "http://localhost:3306/administradores"
-export const URL_ADMINISTRADORES_CREAR = "http://localhost:3306/administradores/create/"
-export const URL_ADMINISTRADORES_EDITAR = "http://localhost:3306/administradores/edit/"
-export const URL_ADMINISTRADORES_ELIMINAR = "http://localhost:3306/administradores/delete/"
+export const URL_ADMINISTRADORES = "https://portfolio-dev-utn-chocobar-matias.onrender.com/administradores"
+export const URL_ADMINISTRADORES_CREAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/administradores/create/"
+export const URL_ADMINISTRADORES_EDITAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/administradores/edit/"
+export const URL_ADMINISTRADORES_ELIMINAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/administradores/delete/"
 
 //Certificaciones
-export const URL_CERTIFICACIONES = "http://localhost:3306/certificados/"
-export const URL_CERTIFICACIONES_CREAR = "http://localhost:3306/certificados/create/"
-export const URL_CERTIFICACIONES_EDITAR = "http://localhost:3306/certificados/edit/"
-export const URL_CERTIFICACIONES_ELIMINAR = "http://localhost:3306/certificados/delete/"
+export const URL_CERTIFICACIONES = "https://portfolio-dev-utn-chocobar-matias.onrender.com/certificados/"
+export const URL_CERTIFICACIONES_CREAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/certificados/create/"
+export const URL_CERTIFICACIONES_EDITAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/certificados/edit/"
+export const URL_CERTIFICACIONES_ELIMINAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/certificados/delete/"
 
 //Contactame
-export const URL_CONTACTAME = "http://localhost:3306/contactame"
-export const URL_CONTACTAME_CREAR = "http://localhost:3306/contactame/create/"
-export const URL_CONTACTAME_EDITAR = "http://localhost:3306/contactame/edit/"
-export const URL_CONTACTAME_ELIMINAR = "http://localhost:3306/contactame/delete/"
+export const URL_CONTACTAME = "https://portfolio-dev-utn-chocobar-matias.onrender.com/contactame"
+export const URL_CONTACTAME_CREAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/contactame/create/"
+export const URL_CONTACTAME_EDITAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/contactame/edit/"
+export const URL_CONTACTAME_ELIMINAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/contactame/delete/"
 
 //Experiencias
-export const URL_EXPERIENCIAS = "http://localhost:3306/experiencias"
-export const URL_EXPERIENCIAS_CREAR = "http://localhost:3306/experiencias/create/"
-export const URL_EXPERIENCIAS_EDITAR = "http://localhost:3306/experiencias/edit/"
-export const URL_EXPERIENCIAS_ELIMINAR = "http://localhost:3306/experiencias/delete/"
+export const URL_EXPERIENCIAS = "https://portfolio-dev-utn-chocobar-matias.onrender.com/experiencias"
+export const URL_EXPERIENCIAS_CREAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/experiencias/create/"
+export const URL_EXPERIENCIAS_EDITAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/experiencias/edit/"
+export const URL_EXPERIENCIAS_ELIMINAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/experiencias/delete/"
 
 //Formaciones
-export const URL_FORMACIONES = "http://localhost:3306/formaciones"
-export const URL_FORMACIONES_CREAR = "http://localhost:3306/formaciones/create/"
-export const URL_FORMACIONES_EDITAR = "http://localhost:3306/formaciones/edit/"
-export const URL_FORMACIONES_ELIMINAR = "http://localhost:3306/formaciones/delete/"
+export const URL_FORMACIONES = "https://portfolio-dev-utn-chocobar-matias.onrender.com/formaciones"
+export const URL_FORMACIONES_CREAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/formaciones/create/"
+export const URL_FORMACIONES_EDITAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/formaciones/edit/"
+export const URL_FORMACIONES_ELIMINAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/formaciones/delete/"
 
 //Idiomas
-export const URL_IDIOMAS = "http://localhost:3306/idiomas"
-export const URL_IDIOMAS_CREAR = "http://localhost:3306/idiomas/create/"
-export const URL_IDIOMAS_EDITAR = "http://localhost:3306/idiomas/edit/"
-export const URL_IDIOMAS_ELIMINAR = "http://localhost:3306/idiomas/delete/"
+export const URL_IDIOMAS = "https://portfolio-dev-utn-chocobar-matias.onrender.com/idiomas"
+export const URL_IDIOMAS_CREAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/idiomas/create/"
+export const URL_IDIOMAS_EDITAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/idiomas/edit/"
+export const URL_IDIOMAS_ELIMINAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/idiomas/delete/"
 
 //Proyectos
-export const URL_PROYECTOS = "http://localhost:3306/proyectos"
-export const URL_PROYECTOS_CREAR = "http://localhost:3306/proyectos/create/"
-export const URL_PROYECTOS_EDITAR = "http://localhost:3306/proyectos/edit/"
-export const URL_PROYECTOS_ELIMINAR = "http://localhost:3306/proyectos/delete/"
+export const URL_PROYECTOS = "https://portfolio-dev-utn-chocobar-matias.onrender.com/proyectos"
+export const URL_PROYECTOS_CREAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/proyectos/create/"
+export const URL_PROYECTOS_EDITAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/proyectos/edit/"
+export const URL_PROYECTOS_ELIMINAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/proyectos/delete/"
 
 //Skills
-export const URL_SKILLS = "http://localhost:3306/skills"
-export const URL_SKILLS_CREAR = "http://localhost:3306/skills/create/"
-export const URL_SKILLS_EDITAR = "http://localhost:3306/skills/edit/"
-export const URL_SKILLS_ELIMINAR = "http://localhost:3306/skills/delete/"
+export const URL_SKILLS = "https://portfolio-dev-utn-chocobar-matias.onrender.com/skills"
+export const URL_SKILLS_CREAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/skills/create/"
+export const URL_SKILLS_EDITAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/skills/edit/"
+export const URL_SKILLS_ELIMINAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/skills/delete/"
 
 //Usuarios
-export const URL_USUARIOS = "http://localhost:3306/usuarios"
-export const URL_USUARIOS_CREAR = "http://localhost:3306/usuarios/create/"
-export const URL_USUARIOS_EDITAR = "http://localhost:3306/usuarios/edit/"
-export const URL_USUARIOS_ELIMINAR = "http://localhost:3306/usuarios/delete/"
+export const URL_USUARIOS = "https://portfolio-dev-utn-chocobar-matias.onrender.com/usuarios"
+export const URL_USUARIOS_CREAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/usuarios/create/"
+export const URL_USUARIOS_EDITAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/usuarios/edit/"
+export const URL_USUARIOS_ELIMINAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/usuarios/delete/"
 
 // Login
-export const URL_LOGIN = "http://localhost:3306/login"
+export const URL_LOGIN = "https://portfolio-dev-utn-chocobar-matias.onrender.com/login"
 
 // Recuperar contraseña
-export const URL_RECUPERAR = "http://localhost:3306/recuperar"
+export const URL_RECUPERAR = "https://portfolio-dev-utn-chocobar-matias.onrender.com/recuperar"
